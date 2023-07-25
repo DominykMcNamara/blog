7c0b7a672990f5a829231a8972856f68e7f22757
DominykMcNamara <dominyksmith@gmail.com>
2023-07-24 22:06:24 -0400

    work on login func

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,10 @@
+import type { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials'
+        })
+    ]
+}
