5063c31b88f153a5131e4a107dc176dc8e66f68f
DominykMcNamara <dominyksmith@gmail.com>
2023-07-26 13:16:11 -0400

    set strict mode to false

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,52 @@
-import  NextAuth  from 'next-auth';
+import  NextAuth, { RequestInternal }  from 'next-auth';
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";     
+import { PrismaClient } from "@prisma/client";
 import { Adapter } from 'next-auth/adapters';
+const bcrypt = require("bcrypt");
+import prisma from '../../../lib/prisma';
 
-const prisma: PrismaClient = new PrismaClient();
+
 
 export default NextAuth({
-    adapter: PrismaAdapter(prisma) as Adapter,
-    providers:[]
+    adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+  providers: [
+    CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+        email: {
+            label: 'username',
+            type: 'email',
+            placeholder: 'email@email.com'
+        },
+        password: {
+            label: 'Password',
+            type: 'password',
+            placeholder: 'Secret123'
+        }
+    },
+       async authorize(credentials) {
+         
+            const user = await prisma?.user.findUnique({
+                where: {
+                    email: credentials?.email
+                }
+            })
+            if (!user) {
+                return null
+            }
+            const hashedPassword = await bcrypt.hash(credentials?.password, 10);
+            const passwordCheck = await bcrypt.compare(hashedPassword, user?.password)
+            if(!passwordCheck) {
+                return null
+            }
+           if(user) {
+            return { ...user }
+           }
+             
+        }
+    })
+  ]
 }) 
 
 
